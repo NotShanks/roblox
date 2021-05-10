f28a3d50c27c3669b47318df74b9cd9c473777a2
NotShanks <70460289+NotShanks@users.noreply.github.com>
2021-05-10 16:29:39 -0400

    Create configedit.lua

---
+++ configedit.lua
@@ -0,0 +1,11 @@
+--merely an example
+local c = require(game.ReplicatedStorage.Config)
+for i,v in pairs(c) do
+    if type(v) == "table" then
+        for i,v in pairs(v) do
+            if v.placeCooldown then
+                v.placeCooldown = 0
+            end
+        end
+    end
+end
