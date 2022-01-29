d1879e4a602ce18c2ca80a984b967c3abc554dc1
NotShanks <70460289+NotShanks@users.noreply.github.com>
2022-01-29 16:00:20 -0500

    Create cookfoods.lua

---
+++ gaylegendsre/cookfoods.lua
@@ -0,0 +1,15 @@
+_G.b = true
+while _G.b do
+wait(1.8)
+local a1 = "RawTrout"
+local a2 = "RawSalmon"
+local a3 = "RawRainbowFish"
+local a4 = "RawSailfish"
+game:GetService("Players").LocalPlayer.PlayerGui.Parkour.Script.CookDish:InvokeServer(a1)
+wait(1.8)
+game:GetService("Players").LocalPlayer.PlayerGui.Parkour.Script.CookDish:InvokeServer(a2)
+wait(1.8)
+game:GetService("Players").LocalPlayer.PlayerGui.Parkour.Script.CookDish:InvokeServer(a3)
+wait(1.8)
+game:GetService("Players").LocalPlayer.PlayerGui.Parkour.Script.CookDish:InvokeServer(a4)
+end
