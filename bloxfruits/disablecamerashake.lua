96dec8a16e79a70e1504c4dba0561a073f0aa2ab
NotShanks <70460289+NotShanks@users.noreply.github.com>
2023-02-26 20:08:52 -0500

    Create disablecamerashake.lua

---
+++ bloxfruits/disablecamerashake.lua
@@ -0,0 +1,8 @@
+local get = require(game:GetService("ReplicatedStorage").Util.CameraShaker)
+local returnnil = function() return nil end
+get.StartShake = returnnil
+get.ShakeOnce = returnnil
+get.ShakeSustain = returnnil
+get.CameraShakeInstance = returnnil
+get.Shake = returnnil
+get.Start = returnnil
