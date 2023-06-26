9a39900c00666e80adc98f368b65d0ef5f024323
NotShanks <70460289+NotShanks@users.noreply.github.com>
2023-06-26 00:47:26 -0400

    Update cutgrass.lua

--- mml2/cutgrass.lua
+++ mml2/cutgrass.lua
@@ -1,10 +1,9 @@
 _G.c = true
-local char = game.Players.LocalPlayer.Character
-local tool = char:FindFirstChildOfClass("Tool")
-while wait(.2) and _G.c do
+while wait(.2) and _G.c do pcall(function()
     for i,v in pairs(workspace.GRASS:GetChildren()) do
-        if (v.CFrame.p - char.HumanoidRootPart.CFrame.p).magnitude < 10 then 
-            workspace["Heck Yeah"].BringTheMowingIGuess:FireServer(v, tool)
+        if (v.CFrame.p - game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame.p).magnitude < 10 then 
+            workspace["Heck Yeah"].BringTheMowingIGuess:FireServer(v, game.Players.LocalPlayer.Character:FindFirstChildOfClass("Tool"))
         end
       end
-    end
+    end)
+end
