8c8ed56fdd862d59089b1136975941562be5a284
NotShanks <70460289+NotShanks@users.noreply.github.com>
2023-06-26 00:47:59 -0400

    Update cutgrass.lua

--- mml2/cutgrass.lua
+++ mml2/cutgrass.lua
@@ -1,7 +1,7 @@
 _G.c = true
 while wait(.2) and _G.c do pcall(function()
     for i,v in pairs(workspace.GRASS:GetChildren()) do
-        if (v.CFrame.p - game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame.p).magnitude < 10 then 
+        if (v.CFrame.p - game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame.p).magnitude < 15 then 
             workspace["Heck Yeah"].BringTheMowingIGuess:FireServer(v, game.Players.LocalPlayer.Character:FindFirstChildOfClass("Tool"))
         end
       end
